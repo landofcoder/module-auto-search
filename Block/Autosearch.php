67d27072d6c06cb117f58b7090c9d5bd31d7fa14
landofcoder <landofcoder@gmail.com>
2021-09-10 14:09:14 +0700

    add new option max category level

--- Block/Autosearch.php
+++ Block/Autosearch.php
@@ -94,7 +94,8 @@ class Autosearch extends \Magento\Catalog\Block\Product\AbstractProduct implemen
 			$searchCollection = "";
 			if($this->_autosearchHelper->getConfig('general/show_filter_category')) {
 				$rootCatId = $this->_storeManager->getStore()->getRootCategoryId();
-				$categories = $this->getTreeCategories($rootCatId, 0);
+				$maxLevel = $this->_autosearchHelper->getConfig('general/max_category_level');
+				$categories = $this->getTreeCategories($rootCatId, 0, (int)$maxLevel);
 			}
 
 			$this->assign( "categories_links", $this->_categories_links);
@@ -165,7 +166,7 @@ class Autosearch extends \Magento\Catalog\Block\Product\AbstractProduct implemen
 	/**
 	 * @return string
 	 */
-	public function getTreeCategories($parentId,$level = 0, $caret = '  '){
+	public function getTreeCategories($parentId,$level = 0, $caret = '  ', $maxLevel = 3){
 		$category_id = $this->getRequest()->getParam("cat");
 		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 		$current_category = $objectManager->get('Magento\Framework\Registry')->registry('current_category');
@@ -197,8 +198,8 @@ class Autosearch extends \Magento\Catalog\Block\Product\AbstractProduct implemen
 				$subcats = $category->getChildren();
 				$html .= '<option value="'.$category->getId().'" '.($category_id == $category->getId() ? 'selected="selected"':'') .'>'.$prefix.$category->getName().'</option>';
 				$subcats = $category->getChildren();
-				if($subcats != ''){ 
-					$html .= $this->getTreeCategories($category->getId(), (int)$level + 1, $caret.'&nbsp;');
+				if ($subcats != '' && ((int)$level + 1) < $maxLevel) { 
+					$html .= $this->getTreeCategories($category->getId(), (int)$level + 1, $caret.'&nbsp;', $maxLevel);
 				}
 
 			}
